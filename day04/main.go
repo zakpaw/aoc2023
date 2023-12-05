0fc93f8dc98bdf5e897e8ad077cd8555c2c25c23
zakpaw <zakpaw36@gmail.com>
2023-12-05 20:07:33 +0100

    feat: day4 p2

--- day04/main.go
+++ day04/main.go
@@ -12,28 +12,41 @@ import (
 type Scratchcard struct {
     WinningNumbers  []interface{}   `parser:"@Int+"`
     ElfNumbers      []interface{}   `parser:"'|' @Int+"`
-    Winning         int
+    WinCount        int
 }
 
-func (s *Scratchcard) checkNumberOfWins() int {
+func (s *Scratchcard) checkNumberOfWins() {
     winningSet := mapset.NewSetFromSlice(s.WinningNumbers)
     elfSet := mapset.NewSetFromSlice(s.ElfNumbers)
-    return winningSet.Intersect(elfSet).Cardinality()
+    s.WinCount = winningSet.Intersect(elfSet).Cardinality()
 }
 
 func main() {
-    parser := participle.MustBuild[Scratchcard]()
+	parser := participle.MustBuild[Scratchcard]()
 	scanner := bufio.NewScanner(os.Stdin)
 
-    var resultP1 int
+	var resultP1 int
+	var cards []*Scratchcard
+	cardCopies := make(map[int]int)
 	for scanner.Scan() {
-        numbers := strings.Split(scanner.Text(), ":")[1]
-        card, _ := parser.ParseString("", numbers)
+		numbers := strings.Split(scanner.Text(), ":")[1]
+		card, _ := parser.ParseString("", numbers)
+		card.checkNumberOfWins()
+		if card.WinCount > 0 {
+			resultP1 += 1 << (card.WinCount - 1)
+		}
+		cards = append(cards, card)
+		cardCopies[len(cards)-1] = 1
+	}
 
-        winsCount := card.checkNumberOfWins()
-        if winsCount > 0 {
-			resultP1 += 1 << (winsCount - 1)
+	resultP2 := 0
+	for i, card := range cards {
+		resultP2 += cardCopies[i]
+		for winOffset := 1; winOffset <= card.WinCount; winOffset++ {
+			if nextCardIndex := i + winOffset; nextCardIndex < len(cards) {
+				cardCopies[nextCardIndex] += cardCopies[i]
+			}
 		}
 	}
-    fmt.Println(resultP1)
+    fmt.Printf("part1: %d\npart2: %d\n", resultP1, resultP2)
 }
